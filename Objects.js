b6ea416f26abcbea12296686509c9c972f1ad869
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-11-16 12:50:15 -0500

    Feature: Add feature to create random number and position of blocks

--- Objects.js
+++ Objects.js
@@ -1,14 +1,20 @@
+const blockXPositions = [6.66, 68.32, 129.98, 191.64, 253.3, 314.96, 376.62, 438.28];
+
 const Objects = {
+  createUniqRandomNumArr() {
+    const numOf = 8;
+    const arr = [];
+    for (let i = 0; i < numOf; i += 1) {
+      arr.push(Math.floor(Math.random() * 8));
+    }
+    return arr.filter((v, i, a) => a.indexOf(v) === i);
+  },
   createBlocks() {
-    //temp blocks in all x positions
-    blocks.push(new Block(0, 6.66, 5))
-    blocks.push(new Block(0, 68.32, 5))
-    blocks.push(new Block(0, 129.98, 5))
-    blocks.push(new Block(0, 191.64, 5))
-    blocks.push(new Block(0, 253.3, 5))
-    blocks.push(new Block(0, 314.96, 5))
-    blocks.push(new Block(0, 376.62, 5))
-    blocks.push(new Block(0, 438.28, 5))
+    const uniqArr = this.createUniqRandomNumArr();
+    for (let i = 0; i < uniqArr.length; i += 1) {
+      let newBlock = new Block(0, blockXPositions[uniqArr[i]], 5);
+      blocks.push(newBlock);
+    }
   },
   createBalls(timestamp) {
     while (count < numOfBalls && timestamp - startTime > 75) {
