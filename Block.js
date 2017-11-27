86cd5f10048b9def8b44b97ecd8e59239c2f4e09
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-11-27 11:17:08 -0500

    Fix: Color input directly in fill style

--- Block.js
+++ Block.js
@@ -7,7 +7,6 @@ class Block {
     this.width = 55;
     this.height = 55;
     this.border = 3;
-    this.color;
   }
   draw() {
     ctxblocks.beginPath();
@@ -15,8 +14,7 @@ class Block {
     ctxblocks.lineWidth = this.border;
     ctxblocks.strokeRect(this.x + this.border, this.y + this.border, this.width - this.border * 2, this.height - this.border * 2);
     ctxblocks.rect(this.x + this.border, this.y + this.border, this.width - this.border * 2, this.height - this.border * 2);
-    this.selectColor();
-    ctxblocks.fillStyle = this.color;
+    ctxblocks.fillStyle = this.selectColor();
     ctxblocks.fill();
     ctxblocks.closePath();
   }
@@ -34,25 +32,25 @@ class Block {
   selectColor() {
     const percentage = this.number / (numOfBalls * 2);
     if (percentage < 0.1) {
-      this.color = '#E50010';
+      return '#E50010';
     } else if (percentage < 0.2) {
-      this.color = '#E70042';
+      return '#E70042';
     } else if (percentage < 0.3) {
-      this.color = '#EA0075';
+      return '#EA0075';
     } else if (percentage < 0.4) {
-      this.color = '#ED00AA';
+      return '#ED00AA';
     } else if (percentage < 0.5) {
-      this.color = '#F000DF';
+      return '#F000DF';
     } else if (percentage < 0.6) {
-      this.color = '#D000F3';
+      return '#D000F3';
     } else if (percentage < 0.7) {
-      this.color = '#9E00F6';
+      return '#9E00F6';
     } else if (percentage < 0.8) {
-      this.color = '#6A00F9';
+      return '#6A00F9';
     } else if (percentage < 0.9) {
-      this.color = '#3500FC';
+      return '#3500FC';
     } else {
-      this.color = '#0000FF';
+      return '#0000FF';
     }
   }
 }
