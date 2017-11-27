7a0b5d6d575c1e11632b3fbc527de98451697b2f
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-11-27 10:19:00 -0500

    Fix: Change block color to vary in shade depending on current number

--- Block.js
+++ Block.js
@@ -7,7 +7,7 @@ class Block {
     this.width = 55;
     this.height = 55;
     this.border = 3;
-    this.color = 'red';
+    this.color;
   }
   draw() {
     ctxblocks.beginPath();
@@ -15,6 +15,7 @@ class Block {
     ctxblocks.lineWidth = this.border;
     ctxblocks.strokeRect(this.x + this.border, this.y + this.border, this.width - this.border * 2, this.height - this.border * 2);
     ctxblocks.rect(this.x + this.border, this.y + this.border, this.width - this.border * 2, this.height - this.border * 2);
+    this.selectColor();
     ctxblocks.fillStyle = this.color;
     ctxblocks.fill();
     ctxblocks.closePath();
@@ -29,5 +30,20 @@ class Block {
     this.y += 60;
     Boundary.checkGameOver(this);
     this.draw();
+  }
+  selectColor() {
+    if (this.number < numOfBalls) {
+      if (this.number < numOfBalls/2) {
+        this.color = '#7272ff';
+      } else {
+        this.color = '#4747e8';
+      }
+    } else {
+      if (this.number < (numOfBalls + numOfBalls/2)) {
+        this.color = '#3535c6';
+      } else {
+        this.color = '#1717c1';
+      }
+    }
   }
 }
