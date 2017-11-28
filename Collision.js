a268a17f9393bce1893b839d8f786e30ca93b506
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-11-28 12:49:28 -0500

    Refactor: Refactor collision logic for improved collision behavior

--- Collision.js
+++ Collision.js
@@ -1,3 +1,4 @@
+let hitPosition = null;
 const Collision = {
   getDistance(ball, blockX, blockY){
     let xDistance = blockX - ball.x;
@@ -5,42 +6,54 @@ const Collision = {
     return  Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));;
   },
   getMiddleDistance(ball, block){
-    let xMidDistance = block.x + block.width/2 - ball.x;
-    let yMidDistance = block.y + block.width/2 - ball.y;
+    let xMidDistance = block.x + block.width/2 + block.border - ball.x;
+    let yMidDistance = block.y + block.width/2 + block.border - ball.y;
     return Math.sqrt(Math.pow(xMidDistance, 2) + Math.pow(yMidDistance, 2));
   },
   checkDistance(ball, block) {
-    const blockPointsX = [block.x, block.x + block.width];
-    const blockPointsY = [block.y, block.y + block.height];
+    const blockPointsX = [block.x - block.border, block.x + block.width + block.border];
+    const blockPointsY = [block.y - block.border, block.y + block.height + block.border];
     //check all corners for collisions as well as middle
-    if (
-      (this.getDistance(ball, blockPointsX[0], blockPointsY[0]) - ball.radius < 0) ||
-      (this.getDistance(ball, blockPointsX[0], blockPointsY[1]) - ball.radius < 0) ||
-      (this.getDistance(ball, blockPointsX[1], blockPointsY[0]) - ball.radius < 0) ||
-      (this.getDistance(ball, blockPointsX[1], blockPointsY[1]) - ball.radius < 0)
-    ) {
+    if (this.getDistance(ball, blockPointsX[0], blockPointsY[0]) - ball.radius < 0) {
+      hitPosition = 'Left';
+      return true;
+    } else if (this.getDistance(ball, blockPointsX[0], blockPointsY[1]) - ball.radius < 0) {
+      hitPosition = 'Left';
+      return true;
+    } else if (this.getDistance(ball, blockPointsX[1], blockPointsY[0]) - ball.radius < 0) {
+      hitPosition = 'Right';
+      return true;
+    } else if (this.getDistance(ball, blockPointsX[1], blockPointsY[1]) - ball.radius < 0) {
+      hitPosition = 'Right';
       return true;
     } else if (this.getMiddleDistance(ball, block) - ball.radius - block.width/2 < 0) {
+      hitPosition = 'Middle';
       return true;
     } else {
+      hitPosition = null;
       return false;
     }
   },
   resolveCollision(ball, block) {
-    const blockLeft = block.x;
-    const blockRight = block.x + block.width;
-    const blockTop = block.y;
-    const blockBottom = block.y + block.height;
+    const blockLeft = block.x - block.border;
+    const blockRight = block.x + block.width + block.border;
+    const blockTop = block.y - block.border;
+    const blockBottom = block.y + block.height + block.border;
 
     block.number -= 1;
     currentScore += 1;
     View.updateCurrentScore();
 
-    if (ball.x < blockLeft && ball.y > blockTop && ball.y < blockBottom) {
-      ball.dx = -ball.dx;
-    } else if (ball.x > blockRight && ball.y > blockTop && ball.y < blockBottom) {
-      ball.dx = -ball.dx
+    if (hitPosition === 'Middle') {
+      if (ball.x < blockLeft) {
+        ball.dx = -ball.dx;
+      } else if (ball.x > blockRight) {
+        ball.dx = -ball.dx;
+      } else {
+        ball.dy = -ball.dy;
+      }
     } else {
+      ball.dx = -ball.dx;
       ball.dy = -ball.dy;
     }
   },
